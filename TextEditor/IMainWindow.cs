cc731ca6f6b213b97c8076bc77a74418ed760f65
Columbia91 <alpha.z91@mail.ru>
2019-06-06 21:39:53 +0600

    Добавьте файлы проекта.

---
+++ TextEditor/IMainWindow.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextEditor
+{
+    interface IMainWindow
+    {
+        string FilePath { get; }
+        string DataContent { get; set; }
+        void SetSymbolCount(int count);
+        event EventHandler FileOpenClick;
+        event EventHandler FileSaveClick;
+        event EventHandler ContentChanged;
+    }
+}
---
+++ TextEditor/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TextEditor
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, IMainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            openFileButton.Click += new EventHandler(OpenFileButton_Click);
+        }
+        
+        public string FilePath
+        {
+            get { return filePathTextBox.Text; }
+        }
+
+        public string DataContent
+        {
+            get { return contentTextBox.Text; }
+            set { contentTextBox.Text = value; }
+        }
+
+        public event EventHandler FileOpenClick;
+        public event EventHandler FileSaveClick;
+        public event EventHandler ContentChanged;
+
+        public void SetSymbolCount(int count)
+        {
+            symbolCountTextBlock.Text = count.ToString();
+        }
+    }
+}
